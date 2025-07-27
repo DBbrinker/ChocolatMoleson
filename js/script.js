67c0278a298d45745cee4c137170f7fc268942fc
DBbrinker <davis_brink@hotmail.com>
2025-07-27 10:34:27 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,2 +1,9 @@
 echo "console.log('Bienvenue sur mon site web !');" > js/script.js
 
+<script>
+  function toggleMenu() {
+    const menu = document.getElementById('navMenu');
+    menu.style.display = (menu.style.display === 'block') ? 'none' : 'block';
+  }
+</script>
+
